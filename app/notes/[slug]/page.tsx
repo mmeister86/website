35b96ada20ec07eae78926c29c8321b1a659b38e
Matthias Meister <meister.matthias86@gmail.com>
2024-11-09 20:47:27 +0100

    slug

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
-import EditorJS from '@editorjs/editorjs';
-
-
-const editor = new EditorJS();
-
 const Blogpost = () => {
   return (
     <div>Das wird mal ein einzelner Blogpost</div>
